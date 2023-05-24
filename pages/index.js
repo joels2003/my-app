b8ced03fe328fd80db0f842de46b97bdd66975aa
joels2003 <joelsheen.mv@gmail.com>
2023-05-24 12:44:20 +0530

    my first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>
+        {router.query.name ? `Hello, ${router.query.name}` : "Required Name"}
+      </h1>
+    </div>
+  );
+}
